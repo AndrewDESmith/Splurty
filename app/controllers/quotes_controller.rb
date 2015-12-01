b35a24d36be87e46e463b597ea26a7205a39d8af
AndrewDESmith <andrew.d.e.smith@gmail.com>
2015-12-01 21:10:31 +0000

    Test quotes controller for successful and unsuccessful (not found) responses on the quote show page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -12,7 +12,15 @@ class QuotesController < ApplicationController
   end
 
   def about
+  end
 
+  def show
+    # Get the first quote where the id is the value the user is looking for
+    @quote = Quote.where(:id => params[:id]).first
+    # Display an error message of "Not found" if a user navigates to a show page for a quote that doesn't exist.
+    if @quote.blank?
+      render :text => "Not found", :status => :not_found
+    end
   end
 
   private
--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "quote show page" do
+    quote = Quote.create(:author => 'Ken Mazaika', :saying => 'You only yolo once.')
+    # Trigger the show page
+    get :show, :id => quote.id
+    # Verify that the appropriate http status code is given to the browser
+    assert_response :success
+  end
+
+  # Check that a 404 "not found" error message is given when a page doesn't exist
+  test "quote show page, not found" do
+    get :show, :id => 'OMG'
+    assert_response :not_found
+  end
 end
